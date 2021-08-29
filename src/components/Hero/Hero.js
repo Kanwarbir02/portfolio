17bd84f70ca414c2590c4c72ab32524d70787e1a
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-29 15:25:07 +0530

    Hero Section and Animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,21 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nonpadding>
+    <LeftSection>
+
+      <SectionTitle main center>
+        I'm Kanwarbir <br />
+        A Full Stack Web Developer
+      </SectionTitle>
+
+      <SectionText>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo in natus, dolorum consequuntur asperiores sed harum quaerat minima minus similique consequatur commodi blanditiis. Cupiditate corporis voluptatibus quos, suscipit expedita consequuntur.
+      </SectionText>
+
+      <Button onClick={() => window.location="/"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
