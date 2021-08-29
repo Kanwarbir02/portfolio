09347224d6a97974d95d16eaf5cfa8f1aa71a654
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-29 19:57:48 +0530

    Projects Section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,29 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+ <Section nopadding id="projects">
+
+   <SectionDivider />
+
+    <SectionTitle main>My Projects</SectionTitle>
+
+    <GridContainer>
+      {projects.map((project) => (
+        <BlogCard key={project.id}>
+          <Img src={project.image}></Img>
+
+          <TitleContent>
+            <HeaderThree>{project.title}</HeaderThree>
+            <Hr></Hr>
+
+            <CardInfo>{project.description}</CardInfo>
+          </TitleContent>
+          
+        </BlogCard>
+      ))}
+    </GridContainer>
+
+ </Section>
 );
 
 export default Projects;
