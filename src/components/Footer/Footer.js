0be0e125e098f9d2f7b7d8fdd8a6460a1091df5c
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-08-30 22:17:57 +0530

    Footer Details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,41 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      
+        <LinkItem>
+
+          <LinkList>
+            <LinkColumn>
+              <LinkTitle>Call</LinkTitle>
+              <LinkItem href="tel:xxxxxxxxxxxx">xxxxxxxxx</LinkItem>
+
+              <LinkTitle>Email</LinkTitle>
+              <LinkItem href="mailto:ksingh6_be20@thapar.edu">ksingh6_be20@thapar.edu</LinkItem>
+            </LinkColumn>
+
+          </LinkList>
+        
+        
+      </LinkItem>
+
+      <SocialContainer>
+      <SocialIcons href="https://github.com/Kanwarbir02">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+
+        <SocialIcons href="https://www.linkedin.com/in/kanwarbir-singh-2ba460200/">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+
+        <SocialIcons href="https://www.instagram.com/kanwarbir02/">
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+        
+      </SocialContainer>
+    </FooterWrapper>
+
+    
   );
 };
 
